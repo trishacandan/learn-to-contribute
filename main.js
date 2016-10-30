0e16f05f16c0e9b9a8ced617d6fe12ba41bb0934
Trisha <trisha.candan@gmail.com>
2016-10-30 22:02:30 +0000

    remove x variable.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   // write your code below here
-  var x = 53;
   var name="Dean"; 
   console.log(name)
   var fruit= ["apple", "banana", "orange"]
